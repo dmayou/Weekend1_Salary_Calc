b405d3efcc4e778a88f4102bee4ae2a1ae270b10
David Mayou <davidpmayou@gmail.com>
2018-11-09 15:21:00 -0600

    adds submit button handler, class constructor

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-console.log('js');
+'use strict';
 
-$(document).ready( ()=>console.log('jq') );
+$(document).ready( onReady );
+
+function onReady() {
+    $('#submitBtn').on('click', submitClicked);
+}
+
+class Employee {
+    constructor(firstName, lastName, idNumber, jobTitle, annualSalary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = idNumber;
+        this.jobTitle = jobTitle;
+        this.annualSalary = annualSalary;
+    }
+}
+
+function submitClicked() {
+    console.log('in submitClicked');
+    console.log(new Employee (
+            $('#fnIn').val(),
+            $('#lnIn').val(),
+            $('#idIn').val(),
+            $('#jobTitleIn').val(),
+            $('#annSalaryIn').val()
+        )
+    );
+
+}
